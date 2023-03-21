78a8544d4e643d1578415f3d876e2869242d0450
Matthew <sile16@gmail.com>
2023-03-21 11:41:40 -0500

    updated fio params.

--- fio/setup.sh
+++ fio/setup.sh
@@ -18,11 +18,11 @@ yum install -y openssh-server git nfs-utils fio
 git clone https://github.com/sile16/fbcp.git
 cd fbcp
 mkdir /mnt/fb200
-mount -t nfs -o vers=3 192.168.20.20:/data /mnt/fb200
+mount -t nfs -o vers=3,nconnect=16 192.168.20.20:/data /mnt/fb200
 ./fbcp -checkmount /mnt/fb200
 
 # run manual fio to test network
-# fio --numjobs=16 --size 100M --nrfiles 4 --group_reporting libnfs_write.fio
-# fio --numjobs=16 --size 100M --nrfiles 4 --group_reporting libnfs_read.fio
+# fio --numjobs=16 --filesize 100M --nrfiles 4 --iodepth 100 --blocksize 524288 --group_reporting libnfs_write.fio
+# fio --numjobs=16 --filesize 100M --nrfiles 4 --iodepth 100 --blocksize 524288 --group_reporting libnfs_read.fio
 
 
