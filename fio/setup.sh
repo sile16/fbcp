5823afc686e2fad963c08864c7dfd1136c727c61
Matthew <sile16@gmail.com>
2023-03-21 00:49:08 -0500

    local commits

--- fio/setup.sh
+++ fio/setup.sh
@@ -8,9 +8,14 @@ yum upgrade -y
 #install openssh-server, git, and nfs-utils
 yum install -y openssh-server git nfs-utils fio
 
-# go
+# golang install
 # wget https://go.dev/dl/go1.20.2.linux-amd64.tar.gz
-#  rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
+# rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
 # export PATH=$PATH:/usr/local/go/bin
 # echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
 # go version
+
+git clone https://github.com/sile16/fbcp.git
+cd fbcp
+mkdir /mnt/fb200
+    mount -t nfs -o vers=3 192.168.20.20:/data /mnt/fb200
